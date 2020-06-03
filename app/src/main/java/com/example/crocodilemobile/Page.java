e8ee870af61b545392c047cde1263a37022cdbc9
Ilias Zalyalov <Zalyalovir@gmail.com>
2020-06-03 20:24:04 +0300

    Попытка создания перехода по ссылке, ссылка не работате по IP, на прямую рработает

--- app/src/main/java/com/example/crocodilemobile/Page.java
+++ app/src/main/java/com/example/crocodilemobile/Page.java
@@ -1,5 +1,11 @@
 package com.example.crocodilemobile;
 
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,20 +13,42 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.provider.Browser;
 import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class Page extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page);
+
+        WebView webView = findViewById(R.id.web);
+        webView.loadUrl("http://178.128.242.32/test");
+
+        WebViewClient webViewClient = new WebViewClient(){
+            @SuppressWarnings("deprecation")
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+            @TargetApi(Build.VERSION_CODES.N)
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request){
+                view.loadUrl(request.getUrl().toString());
+                return true;
+            }
+      };
+            webView.setWebViewClient(webViewClient);
     }
-
-    
-
-
-
-
 }
