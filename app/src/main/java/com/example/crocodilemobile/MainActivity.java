4cf48476c51fcff1667740b9355f1e7e14719d5a
Ilias Zalyalov <Zalyalovir@gmail.com>
2020-06-03 15:33:24 +0300

    Создан переход на Activity (Page)

--- app/src/main/java/com/example/crocodilemobile/MainActivity.java
+++ app/src/main/java/com/example/crocodilemobile/MainActivity.java
@@ -2,18 +2,33 @@ package com.example.crocodilemobile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1 = findViewById(R.id.btn1);
+        btn1.setOnClickListener(this);
+
+    }
+    public void onClick (View v) {
+        switch (v.getId()) {
+            case R.id.btn1:
+                Intent intent = new Intent(this, Page.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 
-
-
-
-    
 }
