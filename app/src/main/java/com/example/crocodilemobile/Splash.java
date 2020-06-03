5a5b50d9ca61c0a2c66c8b1f8fa00d1b147a271a
Ilias Zalyalov <Zalyalovir@gmail.com>
2020-06-03 13:42:20 +0300

    Создание проекта, запуск Splash

---
+++ app/src/main/java/com/example/crocodilemobile/Splash.java
@@ -0,0 +1,42 @@
+package com.example.crocodilemobile;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.concurrent.TimeUnit;
+
+public class Splash extends Activity {
+
+    private final int SPLASH_DISPLEY_LEHGHT = 5000;// время активити Splash 5000 ms
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                Splash.this.startActivity(intent);
+                Splash.this.finish();
+            }
+        }, SPLASH_DISPLEY_LEHGHT);
+
+    }
+
+        @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        }
+
+
+
+}
+
