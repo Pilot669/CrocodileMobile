26032da331bde690eed4841ee6a66670ee7afa43
Ilyas Zalyalov <zalyalovir@gmail.com>
2021-01-01 12:51:29 +0300

    Изменение

---
+++ app/src/main/java/com/example/crocodilemobile/Locations.java
@@ -0,0 +1,10 @@
+package com.example.crocodilemobile;
+
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
+
+public class Locations {
+
+
+}
